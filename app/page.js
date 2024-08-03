373506104087595a6a40217ecbe43cf438c5d195
raglandconnor <raglandconnor@ufl.edu>
2024-08-03 18:44:21 -0400

    Add search page template layout

--- app/page.js
+++ app/page.js
@@ -143,6 +143,44 @@ export default function DashboardPage() {
                     <div className="grid w-full gap-4 mx-4">
                       <Card>
                         <CardHeader>
+                          <div className="md:flex md:flex-row md:justify-between">
+                            <div>
+                              <CardTitle>Item 1</CardTitle>
+                              <CardDescription>
+                                Item description
+                              </CardDescription>
+                            </div>
+                            <div className="mt-4 md:mt-0 flex flex-row gap-2">
+                              <Button>Add</Button>
+                              <Button>Remove</Button>
+                            </div>
+                          </div>
+                        </CardHeader>
+                        <CardContent>image goes here</CardContent>
+                      </Card>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            </TabsContent>
+            <TabsContent value="search" className="space-y-4">
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
+                <div className="lg:col-start-3 col-span-4 flex flex-row gap-2">
+                  <Search />
+                  <Button>Search</Button>
+                </div>
+              </div>
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
+                <Card className="lg:col-start-3 col-span-4">
+                  <CardHeader>
+                    <CardTitle>Items</CardTitle>
+                  </CardHeader>
+                  <CardContent className="pl-2 pr-10 md:pl-4 md:pr-12">
+                    {/* <Overview /> */}
+                    {/* Items go here. */}
+                    <div className="grid w-full gap-4 mx-4">
+                      <Card>
+                        <CardHeader>
                           <div className="flex flex-row justify-between">
                             <div>
                               <CardTitle>Item 1</CardTitle>
@@ -159,20 +197,6 @@ export default function DashboardPage() {
                         <CardContent>image goes here</CardContent>
                       </Card>
                     </div>
-                  </CardContent>
-                </Card>
-              </div>
-            </TabsContent>
-            <TabsContent value="search" className="space-y-4">
-              <Search />
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-                <Card className="col-span-4">
-                  <CardHeader>
-                    <CardTitle>Items</CardTitle>
-                  </CardHeader>
-                  <CardContent className="pl-2">
-                    {/* <Overview /> */}
-                    {/* Items go here. */}
                   </CardContent>
                 </Card>
               </div>
